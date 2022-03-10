5e3b21ba1a69351bf0fabf2f94e650b38b822eae
NielsVdBossche <nielsvdbossche@gmail.com>
2022-03-10 14:47:22 +0100

    plotscript

---
+++ CutflowPlot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# combine datacards
+
+eras=("2016PreVFP" "2016PostVFP" "2017" "2018")
+process=("TTTT" "TTZ" "TTW" "TTH" "ttbar" "Xgamma" "Rare" "TTVV") 
+
+filestring=""
+
+for e in ${eras[@]}; do
+    for p in ${process[@]}; do
+        file=$(ls -t ../ewkino/_FourTopAnalysis/Output/Cutflow*$(e)*$(p)* | head -1)
+        filestring=${filestring}file
+        filestring=${filestring}" "
+    done
+done
+
+./stacker_exec $filestring SettingFiles/cutflow.txt
