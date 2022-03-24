dbfd6be5b51012a5f9e446d803a8d479b0c6fdf4
NielsVdBossche <nielsvdbossche@gmail.com>
2022-03-24 16:04:22 +0100

    added new script and fixed old one to work with new formats

--- MCResults.sh
+++ MCResults.sh
@@ -4,7 +4,8 @@
 
 eras=("2016PreVFP" "2016PostVFP" "2017" "2018")
 erasAlt=("2016" "2017" "2018")
-process=("TTTT" "TTZ" "TTW" "TTH" "ttbar" "Xgamma" "Rare" "TTVV") 
+# make  sure this stays the same or find a better solution
+process=("TTTT" "TTZ" "TTW" "TTH" "ttbar" "Xgamma" "Rare_0" "Rare_1" "Rare_2" "Rare_3" "Rare_4" "Rare_5" "TTVV_0" "TTVV_1") 
 
 filestring=""
 
@@ -20,7 +21,7 @@ echo $filestring
 
 ./stacker_exec $filestring SettingFiles/main.txt -unc UncertaintyFiles/full.txt
 
-for e in ${erasAlt[@]}; do
+for e in ${eras[@]}; do
     filestring=""
     for p in ${process[@]}; do
         file=$(ls -t ../ewkino/_FourTopAnalysis/Output/*MCAll*$e*$p* | head -1)
@@ -32,5 +33,5 @@ for e in ${erasAlt[@]}; do
 done
 
 cd combineFiles
-combineCards.py y16Pre=DC_2016.txt y17=DC_2017.txt y18=DC_2018.txt > combinedDatacard.txt
+combineCards.py y16Pre=DC_2016PreVFP.txt y16Post=DC_2016PostVFP.txt y17=DC_2017.txt y18=DC_2018.txt > combinedDatacard.txt
 combine -M Significance combinedDatacard.txt -t -1 --expectSignal=1
