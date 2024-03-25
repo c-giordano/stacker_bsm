7d3e0778f9b374fe89d7967dcc8548081f8e97c5
NielsVdBossche <nielsvdbossche@gmail.com>
2024-03-25 14:14:08 +0100

    EFT weight creation

--- pythonStacker/createEFTWeights.py
+++ pythonStacker/createEFTWeights.py
@@ -6,41 +6,54 @@ from plugins.eft import eft_reweighter
 import awkward as ak
 import uproot
 import argparse
+import os
+
+from createHistograms import get_tree_from_file
 
 
-def get_eftvariations_filename(filename: str, eventclass) -> str:
-    return "output/eftWeights/evClass_" + str(eventclass) + "_wgts.parquet"
+def arguments():
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument("-f", "--file", dest="inputfile", action="store", required=True,
+                        help="Inputfile for which variations must be stored")
+    parser.add_argument("-e", "--eventClass", dest="eventclass", action="store", default=11,
+                        help="which event class to check")
+    parser.add_argument("--storage", dest="storage", type=str,
+                        default="Intermediate", help="Path at which the histograms are stored")
+
+    opts, opts_unknown = parser.parse_known_args()
+    return opts
 
 
-def reweight_and_write(reweighter, eventclass, tree):
+def get_eftvariations_filename(storage: str, filename: str, eventclass) -> str:
+    return os.path.join(storage, "eftWeights", "evClass_" + str(eventclass) + "_wgts.parquet")
+
+
+def reweight_and_write(reweighter, eventclass, tree, storage):
     arrs = tree.arrays(["eftVariationsNorm"], "eventClass==" + str(eventclass), aliases={"eftVariationsNorm": "eftVariations/eftVariations[:,0]"})
+
+    # should become a record rather than a single array, using the naming scheme I devised in the init
 
     var = ak.Array(reweighter.transform_weights(entry[1:]) for entry in arrs.eftVariationsNorm)
 
     if len(var) == 0:
         return
 
-    outputfile = get_eftvariations_filename("", eventclass)
+    outputfile = get_eftvariations_filename(storage, "", eventclass)
     ak.to_parquet(var, outputfile)
     return
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description=__doc__)
-
-    parser.add_argument("-f", "--file", dest="inputfile", action="store", required=True,
-                        help="Inputfile for which variations must be stored")
-    parser.add_argument("-e", "--eventClass", dest="eventclass", action="store", default=-1,
-                        help="which event class to check")
-
-    opts, opts_unknown = parser.parse_known_args()
+    args = arguments()
 
     reweighter = eft_reweighter()
-    tree: uproot.TTree = uproot.open(opts.inputfile+":test")
+    # tree = uproot.open(args.inputfile)
 
-    if (int(opts.eventclass) == -1):
+    tree = get_tree_from_file(args.inputfile, "TTTT_EFT")
+    if (int(args.eventclass) == -1):
         for i in range(15):
-            reweight_and_write(reweighter, i, tree)
+            reweight_and_write(reweighter, i, tree, args.storage)
     else:
-        print(f"running weights for class {opts.eventclass}")
-        reweight_and_write(reweighter, int(opts.eventclass), tree)
+        print(f"running weights for class {args.eventclass}")
+        reweight_and_write(reweighter, int(args.eventclass), tree, args.storage)
---
+++ pythonStacker/submitEFTWeightCreation.py
@@ -0,0 +1,49 @@
+import argparse
+import os
+import json
+import glob
+import src.jobSubmission.condorTools as ct
+
+
+def arguments():
+    parser = argparse.ArgumentParser(description=__doc__)
+
+
+    args, args_unknown = parser.parse_known_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = arguments()
+
+    # loop processes and check which one has EFT, then submit it
+    with open(args.processfile, 'r') as f:
+        content = json.load(f)
+        processes = content["processes"]
+        basedir = content["basedir"]
+
+    commandset = []
+    for process in processes:
+        # if has EFT
+        files = []
+        for filebase in process["fileglobs"]:
+            fileglob = os.path.join(basedir, filebase)
+            fileglob += f"*{args.years[0]}"
+            if args.systematic == "weight" or args.systematic is None:
+                fileglob += "*base.root"
+            true_files = glob.glob(fileglob)
+            files.extend(true_files)
+
+        cmds = []
+        for filename in files:
+            if process.get("hasEFT", 0) == 0:
+                continue
+            for eventclass in range(14):
+                cmd = "python3 createEFTWeights.py"
+                cmd += f" -f {filename}"
+                cmd += f" -e {eventclass}"
+                cmds.append(cmd)
+
+        commandset.append(cmds)
+
+    ct.submitCommandsetsAsCondorCluster("CreateEFTWeights", commandset, scriptfolder="Scripts/condor/")
