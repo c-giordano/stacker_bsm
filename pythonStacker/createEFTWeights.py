On EFT related work: rewritten the EFT weight calculation to be quicker. Fixed the histogram creation. Fixed the plots.

pythonStacker/createEFTWeights.py
@@ -2,13 +2,14 @@
 For files with eft weights, prepare and store the weights per event class, since it takes a lot of computing time.
 """
 
-from plugins.eft import eft_reweighter
+import plugins.eft as eft
+
 import awkward as ak
 import uproot
 import argparse
 import os
 
-from createHistograms import get_tree_from_file
+import src
 
 
 def arguments():
@@ -33,26 +34,37 @@ def get_eftvariations_filename(storage: str, filename: str, eventclass) -> str:
 
 def reweight_and_write(reweighter, eventclass, tree, storage):
     arrs = tree.arrays(["eftVariationsNorm"], "eventClass==" + str(eventclass), aliases={"eftVariationsNorm": "eftVariations/eftVariations[:,0]"})
-
     # should become a record rather than a single array, using the naming scheme I devised in the init
 
-    var = ak.Array(reweighter.transform_weights(entry[1:]) for entry in arrs.eftVariationsNorm)
+    import numpy as np
+    # var = np.array([reweighter.transform_weights(entry[1:]) for entry in arrs.eftVariationsNorm])
+    var = np.transpose(np.array(reweighter.transform_weights_parallel(ak.to_numpy(arrs.eftVariationsNorm[:, 1:]))))
 
     if len(var) == 0:
         return
 
+    # print(var)
+    # print(var_par)
+    # print(np.all(np.isclose(var, var_par)))
+    # exit()
+
+    # postprocess: get names, then make dict, then make record
+    final_prerecord = dict()
+    eft_names = ["Central"] + eft.getEFTVariationsGroomed()
+    for i, name in enumerate(eft_names):
+        final_prerecord[name] = var[:, i]
     outputfile = get_eftvariations_filename(storage, "", eventclass)
-    ak.to_parquet(var, outputfile)
+    ak.to_parquet(ak.Record(final_prerecord), outputfile)
     return
 
 
 if __name__ == "__main__":
     args = arguments()
 
-    reweighter = eft_reweighter()
+    reweighter = eft.eft_reweighter()
     # tree = uproot.open(args.inputfile)
 
-    tree = get_tree_from_file(args.inputfile, "TTTT_EFT")
+    tree = src.get_tree_from_file(args.inputfile, "TTTT_EFT")
     if (int(args.eventclass) == -1):
         for i in range(15):
             reweight_and_write(reweighter, i, tree, args.storage)

pythonStacker/plotHistograms.py
@@ -82,6 +82,12 @@ def modify_yrange(total_content, axis, ratio=False):
     axis.set_ylim((cont_min, cont_max))
 
 
+def modify_yrange_shape(total_content, axis, maxscale=1.8):
+    cont_max = maxscale * np.max(total_content)
+    cont_min = 0.8 * np.min(total_content)
+    axis.set_ylim((cont_min, cont_max))
+
+
 def get_lumi(years):
     total_lumi = 0.
     lumi = {
@@ -97,6 +103,25 @@ def get_lumi(years):
     if total_lumi >= 100.:
         total_lumi = round(total_lumi)
     return total_lumi
+
+
+def generate_outputfolder(years, outputfolder, suffix=""):
+    # outputfolder with year:
+    outputsubfolder = ""
+    if len(years) == 1:
+        outputsubfolder += "_" + years[0]
+    elif len(years) < 4:
+        outputsubfolder += "_" + "_".join(years)
+    else:
+        outputsubfolder += "_Run2"
+
+    outputsubfolder += suffix
+
+    outputfolder_base = os.path.join(outputfolder, outputsubfolder)
+    if not os.path.exists(outputfolder_base):
+        os.makedirs(outputfolder_base)
+    copy_index_html(outputfolder_base)
+    return outputfolder_base
 
 
 def plot_variable_base(variable: Variable, plotdir: str, processes: dict, histograms: dict, storagepath: str, years: list, ratio=True, no_uncertainty=False, shapes=False):  # , samplelist: str, plotdir: str, storagepath: str):
@@ -177,18 +202,8 @@ if __name__ == "__main__":
     storagepath = args.storage
 
     # outputfolder with year:
-    outputsubfolder = ""
-    if len(args.years) == 1:
-        outputsubfolder += "_" + args.years[0]
-    elif len(args.years) < 4:
-        outputsubfolder += "_" + "_".join(args.years)
-    else:
-        outputsubfolder += "_Run2"
+    outputfolder_base = generate_outputfolder(args.years, args.outputfolder)
 
-    outputfolder_base = os.path.join(args.outputfolder, outputsubfolder)
-    if not os.path.exists(outputfolder_base):
-        os.makedirs(outputfolder_base)
-    copy_index_html(outputfolder_base)
     print(channels)
     for channel in channels:
         if args.channel is not None and channel != args.channel:

pythonStacker/plotShapeVariations.py
@@ -15,10 +15,23 @@ from plotHistograms import modify_yrange_shape, generate_outputfolder, copy_inde
 
 import plugins.eft as eft
 
+from submitHistogramCreation import args_add_settingfiles, args_select_specifics
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Process command line arguments.')
     # TODO
+    args_add_settingfiles(parser)
+    args_select_specifics(parser)
+
+    default_output = "/user/nivanden/public_html/Interpretations/Plots/"
+    if os.getenv("CMSSW_VERSION") is None:
+        default_output = "output/"
+    parser.add_argument("-o", "--output", dest="outputfolder", action="store", required=False,
+                        default=default_output, help="outputfolder to use for plots")
+    parser.add_argument("--storage", dest="storage", type=str,
+                    default="Intermediate", help="Path at which the \
+                    histograms are stored")
     # add general specifiers
     # add toggles
     # need specifics for: process, maybe channel? Idk see what nominal plotting does
@@ -32,12 +45,10 @@ def main_plot_EFT(variable: Variable, plotdir: str, histograms, process_info: di
 
     binning = generate_binning(variable.range, variable.nbins)
     # first plot nominal, then start adding variations
-
     nominal_content = np.array(ak.to_numpy(histograms[variable.name]["nominal"]))
-
     # pretty_name = generate_process_name("SM", info)
     nominal_weights = np.ones(len(nominal_content))
-    ax_main.hist(binning[:-1], binning, weights=nominal_weights, histtype="step", color=process_info["color"], label="SM")
+    ax_main.hist(binning[:-1], binning, weights=nominal_weights, histtype="step", color="k", label="SM")
 
     eft_variations = eft.getEFTVariationsLinear()
     minim = 1.
@@ -45,21 +56,23 @@ def main_plot_EFT(variable: Variable, plotdir: str, histograms, process_info: di
     for eft_var in eft_variations:
         lin_name = "EFT_" + eft_var
         quad_name = lin_name + "_" + eft_var
-        current_variation = nominal_content
-        current_variation += np.array(ak.to_numpy(histograms[variable.name][lin_name]["Up"]))
-        current_variation += np.array(ak.to_numpy(histograms[variable.name][quad_name]["Up"]))
 
-        current_variation /= nominal_content
+        current_variation = nominal_content
+        current_variation = current_variation + np.array(ak.to_numpy(histograms[variable.name][lin_name]["Up"]))
+        current_variation = current_variation + np.array(ak.to_numpy(histograms[variable.name][quad_name]["Up"]))
+
+        current_variation = np.nan_to_num(current_variation / nominal_content, nan=1.)
 
         minim = min(minim, np.min(current_variation))
-        maxim = max(maxim, np.min(current_variation))
+        maxim = max(maxim, np.max(current_variation))
         pretty_eft_name = eft_var + " = 1"
         ax_main.hist(binning[:-1], binning, weights=current_variation, histtype="step", label=pretty_eft_name)
 
     ax_main.set_xlim(variable.range)
-    ax_main.set_ylabel("Events")
-    modify_yrange_shape((minim, maxim), ax_main)
-    ax_main.legend(ncols=2)
+    ax_main.set_ylabel("WC / SM")
+    modify_yrange_shape((minim, maxim), ax_main, maxscale=1.4)
+    ax_main.legend(ncol=2)
+    ax_main.set_xlabel(variable.axis_label)
 
     # fix output name
     fig.savefig(os.path.join(plotdir, f"{variable.name}.png"))
@@ -69,6 +82,7 @@ def main_plot_EFT(variable: Variable, plotdir: str, histograms, process_info: di
 
 if __name__ == "__main__":
     args = parse_arguments()
+    np.seterr(divide='ignore', invalid='ignore')
 
     # load process specifics
     # need a set of processes
@@ -109,17 +123,16 @@ if __name__ == "__main__":
 
         for subchannel in channels[channel].subchannels.keys():
             storagepath_tmp = os.path.join(storagepath, channel + subchannel)
-            histograms = dict()
 
             outputfolder = os.path.join(outputfolder_base, channel, subchannel)
             if not os.path.exists(outputfolder):
                 os.makedirs(outputfolder)
-            copy_index_html(outputfolder)
+            # if not os.path.exists(outputfolder):
+            #     os.makedirs(outputfolder)
+            # copy_index_html(outputfolder)
 
-            for process, info in processinfo.items():
-                histograms[process] = dict()
-                for year in args.years:
-                    histograms[process][year] = HistogramManager(storagepath_tmp, process, variables, systematics, year)
-                    histograms[process][year].load_histograms()
+            # for process, info in processinfo.items():
+            histograms = HistogramManager(storagepath_tmp, args.process, variables, systematics, args.years[0])
+            histograms.load_histograms()
             for _, variable in variables.get_variable_objects().items():
                 main_plot_EFT(variable, outputfolder, histograms, processinfo)

pythonStacker/plugins/eft/HyperPoly.py
@@ -75,11 +75,11 @@ class HyperPoly:
         self.N   = len( param_points )
         print(len(param_points))
         # Coordinates
-        self.param_points = param_points 
+        self.param_points = np.array(param_points)
         # Number of variables
         self.nvar = len( param_points[0] ) 
         # Reference point
-        self.ref_point = ref_point if ref_point is not None else tuple([0 for var in range(self.nvar)])
+        self.ref_point = np.array(ref_point) if ref_point is not None else np.array(tuple([0 for var in range(self.nvar)]))
         # Check reference point
         if len(self.ref_point)!=self.nvar:
             logger.error('Reference point has length %i but should have length %i', len(self.ref_point), self.nvar )
@@ -118,15 +118,31 @@ class HyperPoly:
     def get_parametrization( self, weights ): 
         ''' Obtain the parametrization for given weights
         '''
-        if len(weights)!=self.N:
+        # print("original")
+        if len(weights) != self.N:
             raise ValueError( "Need %i weights that correspond to the same number of param_points. Got %i." % (self.N, len(weights)) )
         b = np.array( [ self.wEXT_expectation( weights, self.combination[d] ) for d in range(self.ndof) ] )
+        return np.dot(self.Ainv, b)
+
+    def get_parametrization_parallel( self, weights):
+        ''' Obtain the parametrization for given weights
+        '''
+        # print("parallel")
+        if len(weights[0]) != self.N:
+            raise ValueError( "Need %i weights that correspond to the same number of param_points. Got %i." % (self.N, len(weights)) )
+        b = np.array( [ self.wEXT_expectation_parallel( weights, self.combination[d] ) for d in range(self.ndof) ] )
         return np.dot(self.Ainv, b)
 
     def wEXT_expectation(self, weights, combination ):
         ''' Compute <wEXT ijk...> = 1/Nmeas Sum_meas( wEXT_meas*i_meas*j_meas*k_meas... )
         '''
-        return sum( [ weights[n]*np.prod( [ (self.param_points[n][elem]-self.ref_point[elem]) for elem in combination ] ) for n in range(self.N) ] ) / float(self.N)
+        return sum( [ weights[n] * np.prod( [ (self.param_points[n][elem]-self.ref_point[elem]) for elem in combination ] ) for n in range(self.N) ] ) / float(self.N)
+
+    def wEXT_expectation_parallel(self, weights, combination ):
+        ''' Compute <wEXT ijk...> = 1/Nmeas Sum_meas( wEXT_meas*i_meas*j_meas*k_meas... )
+        '''
+        return np.sum(np.array([weights[:, n] * np.prod([(self.param_points[n][elem]-self.ref_point[elem]) for elem in combination]) for n in range(self.N)] ), axis=0) / float(self.N)
+
 
     def expectation(self, combination ):
         ''' Compute <wEXT ijk...> = 1/Nmeas Sum_meas( i_meas*j_meas*k_meas... )

pythonStacker/src/histogramTools/__init__.py
@@ -24,7 +24,7 @@ class HistogramManager:
                     os.makedirs(self.cache_folder[var])
                 except FileExistsError:
                     print(f"Folder {self.cache_folder[var]} exists. Makedirs failed.")
-                
+
             self.base_name[var] = os.path.join(self.cache_folder[var], filename)
 
             if self.systematics is None:
@@ -46,7 +46,7 @@ class HistogramManager:
         self.histograms[key] = value
 
     def get_name(self, var: str, sys: str = "nominal"):
-        print(self.base_name[var] + sys + ".parquet")
+        # print(self.base_name[var] + sys + ".parquet")
         return self.base_name[var] + sys + ".parquet"
 
     def load_histograms(self):
@@ -73,7 +73,7 @@ class HistogramManager:
             print(f"Note: Variable {var} for systematic {sys} does not exist with base {self.base_name[var]}.")
             content["Up"] = np.zeros(1)
             content["Down"] = np.zeros(1)
-        
+
         # if sys == "nominal" or sys == "stat_unc":
         #     content = ak.from_parquet(self.get_name(var, sys))
         # else: